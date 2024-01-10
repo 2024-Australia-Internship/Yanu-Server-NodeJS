1b1b643793560ddb9dbcf778ddb4faaf92f1e3c8
seonyo <jsy6535@naver.com>
2024-01-11 08:53:00 +0900

    :bug: 회원가입 INSERT 값 변경

--- routes/user-controller.js
+++ routes/user-controller.js
@@ -9,9 +9,9 @@ exports.registerPostMid = (req, res) => {
             console.log("slat에서 에러 발생 : " + err);
             return;
         } else{
-            db.query('INSERT INTO user (user_id, user_pw, user_salt, user_phonenumber) VALUES (?,?,?,?)', [user_id, user_pw, salt,user_phonenumber], (err, result) => {
+            db.query('INSERT INTO user (user_id, user_pw, user_salt, user_phonenumber) VALUES (?,?,?,?)', [user_id, key, salt, user_phonenumber], (err, result) => {
                 if(err){
-                    console.log(err.message);
+                    console.log("회원가입 실패 " + err.message);
                     return res.json({message : '회원가입 실패'});
                 } else {
                     return res.json(result);
@@ -20,3 +20,26 @@ exports.registerPostMid = (req, res) => {
         }
     })
 }
+
+
+exports.loginPostMid = (req, res) => {
+    const { user_id, user_pw } = req.body;
+    let salt = req.salt;
+    crypto.pbkdf2(user_pw, salt, 8745, 64, "sha512", (err, key) => {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            db.query('SELECT user_id, user_pw from user where user_id = ? AND user_pw = ?', [user_id, key], (err, result) => {
+                if (result.length === 1){
+                    console.log(user_id)
+                    return res.json({ message: '로그인 성공' });
+                } else if (result.length === 0){
+                    return res.json({ message: '아이디나 비밀번호가 일치하지 않음' }); 
+                }  else if (err){
+                    return res.json({message : '로그인 실패'});
+                }
+            })
+        }
+    })
+};

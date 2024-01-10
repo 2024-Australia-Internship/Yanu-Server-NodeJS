7e9c8b9edd2d47cd529468fb302bc17f77a6a7f2
seonyo <jsy6535@naver.com>
2024-01-10 09:45:43 +0900

    ✨ db 연결

--- api.js
+++ api.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db/db');
 const app = express();
 const port = 3000;
 
+
 app.use(express.json());
 app.use(cors());
+
+db.getConnection((err) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.message);
+    } else {
+        console.log('Database connected successfully');
+    }
+});
 
 
 app.listen(port, ()=> {

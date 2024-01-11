1491c193625c01f81340baf80d8b3380fd1c9356
seonyo <jsy6535@naver.com>
2024-01-11 14:24:45 +0900

    :white_check_mark: 커밋 테스트

--- middlewares/salt.js
+++ middlewares/salt.js
@@ -1,15 +1,24 @@
-/*const saltRequired = (req, res, next) => {
-    const { user_id, user_pw } = req.body;
-    db.query('SELECT user_salt FROM user where user_id = ?', [user_id], (err, result) => {
-        if (result.length === 1) {
-            const salt = result[0].user_salt.toString();
+const { User } = require('../models');
+
+const saltRequired = async (req, res, next) => {
+    const { user_email } = req.body;
+    try{
+        const user = await User.findOne({
+            where : {user_email},
+            attributes : ['user_salt'],
+        });
+
+        if(user) {
+            const salt = user.user_salt.tostring();
             req.salt = salt;
             next();
         } else {
-            console.log(result.length);
-            res.status(404).json({ message: '아이디가 일치하지 않음' });
+            res.json({message : ' 아이디가 일치하지 않음'});
         }
-    })
-};
+    } catch(error) {
+        console.log('saltedReqruied 미들웨어 오류:', error.message);
+        res.json({message : '서버 오류'});
+    }
+}
 
-module.exports = saltRequired;*/
+module.exports = saltRequired
--- routes/user-controller.js
+++ routes/user-controller.js
@@ -1,41 +1,41 @@
-const {User} = require('../models');
+const { User } = require('../models');
 const crypto = require('crypto');
 
 exports.registerPostMid = async (req, res) => {
-    try{
-        const {user_email, user_pw, user_phonenumber} = req.body;
+    try {
+        const { user_email, user_pw, user_phonenumber } = req.body;
         const salt = crypto.randomBytes(128).toString('base64');
         const hashedPassword = crypto.pbkdf2Sync(user_pw, salt, 8754, 64, 'sha512').toString('hex');
         const newUser = await User.create({
-            user_email : user_email,
-            user_pw : hashedPassword,
-            user_salt : salt,
-            user_phonenumber : user_phonenumber,
+            ...req.body,
+            user_pw: hashedPassword,
         });
-        res.json({message : '회원가입 성공'});
-    } catch (error){
+        res.json({ message: '회원가입 성공' });
+    } catch (error) {
         console.log('회원가입 실패: ', error.message);
-        res.json({mesage : '회원가입 실패'});
+        res.json({ message: '회원가입 실패' });
     }
 };
 
-/*exports.loginPostMid = (req, res) => {
-    const { user_id, user_pw } = req.body;
+exports.loginPostMid = async (req, res) => {
+    const { user_email, user_pw } = req.body;
     let salt = req.salt;
-    crypto.pbkdf2(user_pw, salt, 8745, 64, "sha512", (err, key) => {
-        if (err) {
-            console.log(err);
-            return;
-        } else {
-            db.query('SELECT user_id, user_pw from user where user_id = ? AND user_pw = ?', [user_id, key], (err, result) => {
-                if (result.length === 1){
-                    return res.json(result[0].user_id);     //로그인 성공 시 user_id 정보 보내주기
-                } else if (result.length === 0){
-                    return res.json({ message: '아이디나 비밀번호가 일치하지 않음' }); 
-                }  else if (err){
-                    return res.json({message : '로그인 실패'});
-                }
-            })
+    console.log(user)
+    let hashedPassword = crypto.pbkdf2Sync(user_pw, salt, 8745, 64, 'sha512').toString('hex');
+    const user = await User.findOne({
+        attributes: ['user_email'],
+        where: {
+            user_email: user_email,
+            user_pw: hashedPassword
         }
-    })
-};*/
+    });
+
+    if (user) {
+        res.json({ result: user_email });
+    }
+    else {
+        //console.log(user)
+        res.json({ message: '아이디 또는 비밀번호를 잘못 입력' });
+    }
+
+};

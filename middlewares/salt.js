3bed40d90db3c409925ea9480ad4a526d1a23f0b
seonyo <jsy6535@naver.com>
2024-01-12 22:15:02 +0900

    ✨ 로그인 api 시퀄라이즈로  구현

--- middlewares/salt.js
+++ middlewares/salt.js
@@ -4,19 +4,19 @@ const saltRequired = async (req, res, next) => {
     const { user_email } = req.body;
     try{
         const user = await User.findOne({
-            where : {user_email},
-            attributes : ['user_salt'],
+            attributes: ['user_salt'],
+            where: {user_email}
         });
 
         if(user) {
-            const salt = user.user_salt.tostring();
+            const salt = user.dataValues.user_salt.toString();
             req.salt = salt;
             next();
         } else {
             res.json({message : ' 아이디가 일치하지 않음'});
         }
     } catch(error) {
-        console.log('saltedReqruied 미들웨어 오류:', error.message);
+        console.log('saltedReqruied 미들웨어 오류:', error);
         res.json({message : '서버 오류'});
     }
 }
--- routes/user-controller.js
+++ routes/user-controller.js
@@ -1,15 +1,26 @@
+const { resolve } = require('path');
 const { User } = require('../models');
 const crypto = require('crypto');
+const { rejects } = require('assert');
+const { log } = require('console');
 
 exports.registerPostMid = async (req, res) => {
     try {
         const { user_email, user_pw, user_phonenumber } = req.body;
         const salt = crypto.randomBytes(128).toString('base64');
-        const hashedPassword = crypto.pbkdf2Sync(user_pw, salt, 8754, 64, 'sha512').toString('hex');
+        const hashedPassword = await new Promise((resolve, rejects)=> {
+            crypto.pbkdf2(user_pw, salt, 8754, 64, 'sha512', (err, derivedKey) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(derivedKey.toString('hex'));
+                }
+            });
+        });
         const newUser = await User.create({
             ...req.body,
             user_pw: hashedPassword,
-            user_salt : salt
+            user_salt: salt
         });
         res.json({ message: '회원가입 성공' });
     } catch (error) {
@@ -20,22 +31,34 @@ exports.registerPostMid = async (req, res) => {
 
 exports.loginPostMid = async (req, res) => {
     const { user_email, user_pw } = req.body;
-    let salt = req.salt;
-    let hashedPassword = crypto.pbkdf2Sync(user_pw, salt, 8745, 64, 'sha512').toString('hex');
-    const user = await User.findOne({
-        attributes: ['user_email'],
-        where: {
-            user_email: user_email,
-            user_pw: hashedPassword
+    const salt = req.salt;
+    try {
+        const hashedPassword = await new Promise((resolve, reject) => {
+            crypto.pbkdf2(user_pw, salt, 8754, 64, 'sha512', (err, derivedKey) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(derivedKey.toString('hex'));
+                }
+            });
+        });
+
+        const loginUser = await User.findOne({
+            attributes: ['user_email'],
+            where: {
+                user_pw: hashedPassword
+            }
+        });
+
+        if (loginUser) {
+            console.log("로그인 성공")
+            res.json({ result: user_email });
+        } else {
+            console.log("비밀번호가 일치하지 않음");
+            res.json({ message: '비밀번호가 일치하지 않습니다' });
         }
-    });
-
-    if (user) {
-        res.json({ result: user_email });
+    } catch (error) {
+        console.log("로그인 실패: ", error);
+        res.json({ message: '로그인 실패' });
     }
-    else {
-        console.log(user)
-        res.json({ message: '아이디 또는 비밀번호를 잘못 입력' });
-    }
-
 };

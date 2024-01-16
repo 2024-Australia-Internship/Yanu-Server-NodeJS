cbd53e4838adc9980f71f58c4614a31f08584826
seonyo <jsy6535@naver.com>
2024-01-16 20:13:22 +0900

    ✨ 회원코드 등록 기능 추가

--- routes/user-controller.js
+++ routes/user-controller.js
@@ -3,10 +3,14 @@ const { User } = require('../models');
 const crypto = require('crypto');
 const multer = require('multer');
 const path = require('path');
+const exp = require('constants');
 
+//회원가입
 exports.registerPostMid = async (req, res) => {
     try {
         const { user_email, user_pw, user_phonenumber } = req.body;
+        const user_code = req.code;
+        console.log("user_code : " + user_code);
         const salt = crypto.randomBytes(128).toString('base64');
         const hashedPassword = await new Promise((resolve, rejects) => {
             crypto.pbkdf2(user_pw, salt, 8754, 64, 'sha512', (err, derivedKey) => {
@@ -20,9 +24,9 @@ exports.registerPostMid = async (req, res) => {
         const newUser = await User.create({
             ...req.body,
             user_pw: hashedPassword,
-            user_salt: salt
+            user_salt: salt,
+            user_code: user_code
         });
-        console.log(newUser);
         res.status(201).json({ success: true, message: '회원가입 성공' });
     } catch (error) {
         console.log('회원가입 실패: ', error.message);
@@ -30,6 +34,7 @@ exports.registerPostMid = async (req, res) => {
     }
 };
 
+//로그인
 exports.loginPostMid = async (req, res) => {
     const { user_email, user_pw } = req.body;
     const salt = req.salt;
@@ -64,6 +69,7 @@ exports.loginPostMid = async (req, res) => {
     }
 };
 
+//올바른 이메일을 입력했는지?
 exports.checkEmailPostMid = async (req, res) => {
     const { user_email } = req.body;
     try {
@@ -82,6 +88,7 @@ exports.checkEmailPostMid = async (req, res) => {
     }
 };
 
+//비밀번호 잃어버렸을 때
 exports.forgetPasswordPatchMid = async (req, res) => {
     const { user_email, new_password } = req.body;
     try {
@@ -98,13 +105,12 @@ exports.forgetPasswordPatchMid = async (req, res) => {
 
         const newPassword = await User.update(
             { user_pw: hashedPassword, user_salt: salt },
-            { where: { user_email: user_email } }
+            { where: { user_email } }
         );
+        console.log(newPassword)
         if (newPassword[0] === 1) {
             res.status(201).json({ success: true, message: '비밀번호 변경 성공' });
         } else {
-            console.log(newPassword);
-            console.log(user_email);
             res.status(404).json({ success: false, message: '일치하는 이메일이 없습니다' });
         }
     } catch (error) {
@@ -112,6 +118,7 @@ exports.forgetPasswordPatchMid = async (req, res) => {
     }
 };
 
+//프르필 이미지 업로드
 exports.profilePostMid = async (req, res) => {
     //이미지 저장 디렉토리 설정
     const storage = multer.diskStorage({
@@ -119,26 +126,29 @@ exports.profilePostMid = async (req, res) => {
             cb(null, 'uploads/');
         },
         filename: function (req, file, cb) {
-            console.log(req.body)
-            const nickname = req.body.nickname;
-            const uniqueSuffix = Date.now() + "_profile_" + nickname;
+            const uniqueSuffix = Date.now() + "_profile_";
             cb(null, uniqueSuffix + path.extname(file.originalname));
         }
     });
-
     const upload = multer({ storage: storage }).single('profile_image');
+
     //multer 업로드 함수 호출
-    upload(req, res, async function (err){
-        if(err instanceof multer.MulterError){
+    upload(req, res, async function (err) {
+        console.log(req.body)
+
+        if (err instanceof multer.MulterError) {
             console.log("Multer Error:", err);
-            return res.status(500).json({success: false, message: '이미지 업로드 실패'});
-        } else if (err){
+            return res.status(500).json({ success: false, message: '이미지 업로드 실패' });
+        } else if (err) {
             console.log("Error:", err);
-            return res.status(500).json({success : false, message : '서버 오류 발생'});
+            return res.status(500).json({ success: false, message: '서버 오류 발생' });
         }
 
         const image_url = req.file ? req.file.filename : null;
         res.status(200).json({ success: true, message: '프로필이 성공적으로 업로드되었습니다.', image_url });
     })
+}
 
+//닉네임과 코멘트 입력
+exports.profileInfoPostMid = async (req, res) => {
 }
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const usersMiddleware = require('./user-controller');
 const saltMiddleware = require('../middlewares/salt');
+const userCodeMiddleware = require('../middlewares/user_code');
 
-router.post('/register', usersMiddleware.registerPostMid);
+router.post('/register', userCodeMiddleware, usersMiddleware.registerPostMid);
 router.post('/login', saltMiddleware, usersMiddleware.loginPostMid); 
 router.post('/check/email', usersMiddleware.checkEmailPostMid);
 router.patch('/forget/password', usersMiddleware.forgetPasswordPatchMid);

a2903f9c2467cc936bb325cc6fb89855303dc216
seonyo <jsy6535@naver.com>
2024-01-18 22:26:39 +0900

    ✨ 상품 이미지 업로드하는 api 구현

--- routes/product.js
+++ routes/product.js
@@ -3,5 +3,6 @@ const productsMiddleware = require('./product-controller');
 const createCodeMiddleware = require('../middlewares/create_code');
 
 router.post('/:user_code/create/info', createCodeMiddleware, productsMiddleware.createInfoPostMid);
+router.post('/:user_code/create/image', productsMiddleware.createImagePostMid);
 
 module.exports = router;

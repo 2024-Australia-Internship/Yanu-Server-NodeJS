b2d1090977cf8f2ae0a53d8b82bc162bb645eee0
seonyo <jsy6535@naver.com>
2024-01-22 13:29:07 +0900

    ✨ 하트 삭제하는 api 구현

--- routes/heart-controller.js
+++ routes/heart-controller.js
@@ -18,3 +18,25 @@ exports.heartPostMid = async (req, res) => {
         res.status(500).json({success : false, message : '하트 등록 중 서버 오류 발생'});
     }
 }
+
+exports.heartDeleteMid = async (req, res) => {
+    const product_code = req.params.product_code;
+    const user_code = req.body.user_code;
+
+    try{
+        const deleteHeart = await Heart.destroy({
+            where : {
+                product_code : product_code,
+                user_code : user_code
+            }
+        });
+        if(deleteHeart){
+            res.status(201).json({success: true, message: '하트 삭제 성공'});
+        } else {
+            res.status(400).json({success: false, message: '하트 삭제 중 오류 발생'});
+        }
+    } catch (error){
+        console.log(error);
+        res.status(500).json({message: false, message: '하트 삭제 중 서버 에러 발생'})
+    }
+}
--- routes/heart.js
+++ routes/heart.js
@@ -3,4 +3,5 @@ const heartMiddleware = require('./heart-controller');
 
 router.post('/:product_code', heartMiddleware.heartPostMid);
 router.delete('/:product_code', heartMiddleware.heartDeleteMid);
+
 module.exports = router;

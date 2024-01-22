b27ccfbb8eaf16aedb08b53041a4c409643d8556
seonyo <jsy6535@naver.com>
2024-01-23 08:50:49 +0900

    ♻️ 상품 등록과 상품 이미지등록 ap 병합

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,6 @@ const productsMiddleware = require('./product-controller');
 const createCodeMiddleware = require('../middlewares/create_code');
 
 router.post('/:user_code/create/info', createCodeMiddleware, productsMiddleware.createInfoPostMid);
-router.post('/:user_code/create/image', productsMiddleware.createImagePostMid);
 router.get('/list', productsMiddleware.listGetMid);
 router.get('/:product_code', productsMiddleware.productcodeGetMid);
 

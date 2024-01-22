f26ae5798f0bc8a802236b922266ea2522a04442
seonyo <jsy6535@naver.com>
2024-01-22 11:54:05 +0900

    ✨ 농장 등록할때 farmer 여부 업데이트 기능 추가

--- routes/farm-controller.js
+++ routes/farm-controller.js
@@ -3,16 +3,20 @@ const { Farm, User} = require('../models');
 exports.registerPostMid = async (req, res) => {
     const {user_code, business_name, farmer_name, farm_phonenumber, farm_email, farm_address} = req.body;
     try{
+        const user = await User.findOne({ where: { user_code } });
+
         const registerFarm = await Farm.create({
             ...req.body
         });
 
         if(registerFarm) {
+            await user.update({ is_farmer: true });
             res.status(201).json({success: true, message: '농장 등록 성공'});
         } else{
             res.status(400).json({success: false, message : '농장 등록 중 오류 발생'})
         }
     } catch(error){
+        console.log(error)
         res.status(500).json({success : false, message : '농장 등록 중 서버 오류 발생'});
     }
 }

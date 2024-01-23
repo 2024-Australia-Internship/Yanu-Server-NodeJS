ea0f5b46cfe1920be7727cd5be797021ee604dba
seonyo <jsy6535@naver.com>
2024-01-23 09:14:03 +0900

    ✨ product-controller 롤백 및 이미지 업로드시 product_code 불러옴

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const productsMiddleware = require('./product-controller');
 const createCodeMiddleware = require('../middlewares/create_code');
 
 router.post('/:user_code/create/info', createCodeMiddleware, productsMiddleware.createInfoPostMid);
+router.post('/:user_code/create/image/:product_code', productsMiddleware.createImagePostMid)
 router.get('/list', productsMiddleware.listGetMid);
 router.get('/:product_code', productsMiddleware.productcodeGetMid);
 

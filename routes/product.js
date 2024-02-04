caac89b93fa37ae8daa8fa7967e2bdcad19ca735
seonyo <jsy6535@naver.com>
2024-02-04 21:02:57 +0900

    :fire: create_code 미들웨어 관련 코드 삭제

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const productsMiddleware = require('./product-controller');
-const createCodeMiddleware = require('../middlewares/create_code');
 
-router.post('/:user_code/create/info', createCodeMiddleware, productsMiddleware.createInfoPostMid);
+router.post('/:user_code/create/info', productsMiddleware.createInfoPostMid);
 router.post('/:user_code/create/image/:product_code', productsMiddleware.createImagePostMid)
 router.get('/list', productsMiddleware.listGetMid);
 router.get('/search/:keyword', productsMiddleware.productSearchGetMid);
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const usersMiddleware = require('./user-controller');
 const saltMiddleware = require('../middlewares/salt');
-const userCodeMiddleware = require('../middlewares/create_code');
 
-router.post('/register', userCodeMiddleware, usersMiddleware.registerPostMid);
+router.post('/register', usersMiddleware.registerPostMid);
 router.post('/login', saltMiddleware, usersMiddleware.loginPostMid); 
 router.post('/check/email', usersMiddleware.checkEmailPostMid);
 router.patch('/forget/password', usersMiddleware.forgetPasswordPatchMid);

5f9524d8345c3fb27d232f978280143f2d93db8e
seonyo <jsy6535@naver.com>
2024-03-25 01:45:52 +0900

    🗃️ 테이블 시퀄라이즈 등록

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Sequelize = require('sequelize');
 const User = require('./user');
 const Product = require('./product');
-const Product_Img = require('./product_img');
 const Favorite = require('./favorite');
 const Farm = require('./farm');
+const Review = require('./reviews');
+const Cart = require('./cart');
+const Purchaes_History = require('./purchaes_history');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -14,15 +16,19 @@ db.sequelize = sequelize;
 
 db.User = User;
 db.Product = Product;
-db.Product_Img = Product_Img;
 db.Favorite = Favorite;
 db.Farm = Farm;
+db.Review = Review;
+db.Cart = Cart;
+db.Purchaes_History = Purchaes_History;
 
 User.init(sequelize);
 Product.init(sequelize);
-Product_Img.init(sequelize);
 Favorite.init(sequelize);
 Farm.init(sequelize);
+Review.init(sequelize);
+Cart.init(sequelize);
+Purchaes_History.init(sequelize);
 
 db.User.hasOne(db.Farm, {foreignKey: 'user_id', sourceKey: 'id'});
 db.Farm.belongsTo(db.User, {foreignKey: 'user_id', targetKey:'id'});
